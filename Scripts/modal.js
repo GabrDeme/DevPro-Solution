7148987883bab67dd676c0a06666f042d19d9936
gabrieldemetrio571@gmail.com <gabrieldemetrio571@gmail.com>
2024-04-14 20:54:11 -0300

    header e sombras

--- Scripts/modal.js
+++ Scripts/modal.js
@@ -1,13 +1,33 @@
-function openModal() {
-    const modal = document.getElementById('modal')
-    modal.classList.add('view')
+//function openModal() {
+//    const modal = document.getElementById('modal');
+//    modal.classList.add('view');
+//    console.log(modal);
+//
+//    modal.addEventListener('click', (e) => {
+//        if (e.target.id === 'btn' || !modal.contains(e.target)) {
+//            modal.classList.remove('view');
+//        }
+//    });
+//}
 
-    modal.addEventListener('blur', (e) => {
-        modal.classList.remove('view')
-    })
+
+///////////////////////////////////////////////////////////////////////////
+const modal = document.getElementById('modal'); 
+function openModal() {
+    modal.classList.add('view');
+
     modal.addEventListener('click', (e) => {
-        if(e.target.id == 'btn') {
-        modal.classList.remove('view')
+        if (e.target.id === 'body' || !modal.contains(e.target)) {
+            modal.classList.add('closeModal'); // Adiciona a classe 'closeModal' ao fechar o modal
+            setTimeout(function() {
+                modal.classList.remove('view');
+           }, 500);
         }
-    })
+    });
+
+    // Remova a classe 'closeModal' quando a animação de fechamento for concluída
+    modal.addEventListener('animationend', () => {
+        modal.classList.remove('closeModal');
+    });
 }
+
